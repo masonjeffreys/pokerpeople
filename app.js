3e751698cf4255fa824f4c0623baa7c5a4486452
Jeff Mason <masonjeffreys@gmail.com>
2021-03-07 14:32:38 -0500

    AddedVisionTemplatingForHapi'

--- app.js
+++
@@ -1,9 +0,0 @@
-console.log("Welcome to Poker Night");
-// const Player = require('./src/player');
-// var player1 = Player("jeff");
-// console.log(player1)
-// player1.name = "jim";
-// console.log(player1.name);
-
-const orchestrator = require('./src/orchestrator');
-orchestrator.startGame();
---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Joi = require('@hapi/joi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/new',
+        handler: (request, h) => {
+            return 'new game'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/call',
+        handler: (request, h) => {
+            return 'calling bet'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/bet',
+        handler: (request, h) => {
+            return `betting ${request.query.amount}`
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/fold',
+        handler: (request, h) => {
+            return 'folding'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/check',
+        handler: (request, h) => {
+            return 'check'
+        }
+    });
+
+    // server.route({
+    //     method: 'GET',
+    //     path: '/account/{username}',
+    //     handler: (request, h) => {
+    //         var accountMock = {};
+    //         if (request.params.username == "jeff"){
+    //             accountMock = {
+    //                 username: "jeff",
+    //                 password: "1234",
+    //                 website: "https://jeffmason.me"
+    //             }
+    //         }
+    //         return accountMock;
+    //     }
+    // });
+
+    // server.route({
+    //     method: "POST",
+    //     path: "/account",
+    //     options: {
+    //         validate: {
+    //             payload: Joi.object({
+    //                 firstname: Joi.string().required(),
+    //                 lastname: Joi.string().required(),
+    //                 timestamp: Joi.any().forbidden().default((new Date).getTime())
+    //             })
+    //         }
+    //     },
+    //     handler: (request, h) => {
+    //         return request.payload
+    //     }
+    // });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
