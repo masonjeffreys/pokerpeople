67b44a27196c77a4f34633c82a5d14dd9be0e1bf
Jeff Mason <masonjeffreys@gmail.com>
2021-05-23 17:27:25 -0400

    ImproveTestScripts

--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 Poker Sim
+
+To get started
+
+Pull down master
+
+(if needing a clean start
+delete node_modules)
+npm install
+
+npm run test
+
+npm run start
--- test/example.test.js
+++ test/example.test.js
@@ -1,10 +1,27 @@
+// Example Test Formats to Follow
+
 'use strict';
 
-const Lab = require('@hapi/lab');
-const { expect } = require('@hapi/code');
-const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script();
-const { init } = require('../server');
+const Code = require('@hapi/code'); // Load Lab (test runner that is produced by Hapi people)
+const Lab = require('@hapi/lab'); // Load Lab (test runner that is produced by Hapi people)
 
+
+const { init } = require('../server'); // If the app is required, this is required to start the server
+const { expect } = Code; // Use this instead of Assert
+const { afterEach, beforeEach, describe, it } = exports.lab = Lab.script(); // Defines several constants that are useful for test followup, and notation
+
+// Every test case should have at least one assertion
+
+// Define test case. Params are
+// 1 - description of test case
+// 2 - callback function
+
+// Basic unit test example
+it('returns true when 1 + 1 equals 2', () => {
+    expect(1 + 1).to.equal(2);
+});
+
+// Basic server page call test example
 describe('GET /', () => {
     let server;
     
