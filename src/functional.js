5b82b5a85cd491d1fa8da107509a70f5c2a8e61c
Jeff Mason <masonjeffreys@gmail.com>
2021-09-06 20:21:34 -0400

    ImplementhandleConnAPI

--- src/functional.js
+++ src/functional.js
@@ -1,15 +1,34 @@
 function handleConn(conn, payload){
-    let user = {firstname: "Jeff", lastname: "Mason", userId: 1};
-    let game = {gameId: 1, players: [], state:{}};
-
     // *Authentication
     // get/create user
-    conn['user'] = user;
+    conn['user'] = findOrCreateUser(payload);
     // get/create game
-    conn['game'] = game;
+    conn['game'] = findOrCreateGame(payload);
     // advance game
     // end game
     return conn
+}
+
+function findOrCreateUser(payload){
+    let user;
+    if (payload && 'userId' in payload){
+        // get user from Database
+        user = {firstname: "Database call", lastname: "goes here", userId: payload.userId}
+    } else {
+        user = {firstname: "Jeff", lastname: "Mason", userId: 1};
+    }
+    return user;
+}
+
+function findOrCreateGame(payload){
+    let game;
+    if (payload && 'gameId' in payload){
+        // get user from Database
+        game = {gameId: 0, players: [], state:{}};
+    } else {
+        game = {gameId: 1, players: [], state:{}};
+    }
+    return game;
 }
 
 module.exports.handleConn = handleConn;
